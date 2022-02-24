4cafdab385579a90a0d7f0838a4cbb31e9f9f601
James Ritchie <69557285+Jiminger@users.noreply.github.com>
2022-02-24 15:53:19 -0500

    Delete VendingMachine directory

--- VendingMachine/src/main/java/com/ritchie/james/Main.java
+++
@@ -1,9 +0,0 @@
-package com.ritchie.james;
-
-public class Main {
-
-    public static void main (String[] args){
-        Controller controller = new Controller(new UserInterface(), new VendingMachine());
-        controller.run();
-    }
-}
